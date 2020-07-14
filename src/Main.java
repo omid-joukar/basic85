dc92e0f9bf4927b82cd82375004cbd23a023a5d7
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-14 08:47:51 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Created by KPS on 7/14/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+            String string1 = "Hello how are you?";
+            System.out.println(string1.startsWith("Hello"));
+        }}
+
